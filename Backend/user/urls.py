1f1d31a6dd420855981a5e1f1dae0de95861a2e7
haaris <haarishuda090@gmail.com>
2024-10-18 22:07:24 +0530

    user login registration working ayaan 1 tum dono 0

---
+++ Backend/user/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='user-create'),
+    path('login/', UserLoginView.as_view(), name='user-login'),
+    path("user-details/",UserProfileDetail.as_view(), name="userdetails") 
+    
+]
